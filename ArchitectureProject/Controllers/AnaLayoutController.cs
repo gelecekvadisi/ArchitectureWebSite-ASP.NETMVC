c0b518202b1acb828a322165e384bb4da48b8333
gelecekvadisi <gelecekvadisiorg@gmail.com>
2024-04-01 23:19:52 +0300

    PartialView, Controller, Model, View, Layout, Shared, ActionResult
    PartialView, Controller, Model, View, Layout, Shared, ActionResult

---
+++ ArchitectureProject/Controllers/AnaLayoutController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArchitectureProject.Controllers
+{
+    public class AnaLayoutController : Controller
+    {
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult MenubarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScrollUpPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PlaceholderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ ArchitectureProject/Controllers/DefaultController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArchitectureProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult Banner()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AboutUs()
+        {
+            return PartialView();
+        }
+        public PartialViewResult CountEmployee()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Services()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Project()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Features()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Testimonials()
+        {
+            return PartialView();
+        }
+        public PartialViewResult Teams()
+        {
+            return PartialView();
+        }
+    }
+}
