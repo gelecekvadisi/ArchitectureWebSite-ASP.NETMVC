fed000bc6ad93c5a5a01d196ad7780d496c777ce
gelecekvadisi <gelecekvadisiorg@gmail.com>
2024-04-22 22:11:53 +0300

    Admin temasına giriş, about update
    Admin temasına giriş, about update

--- ArchitectureProject/Controllers/AboutController.cs
+++ ArchitectureProject/Controllers/AboutController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ArchitectureProject.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,15 +7,41 @@ using System.Web.Mvc;
 
 namespace ArchitectureProject.Controllers
 {
-    public class AboutController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public PartialViewResult SloganBanner()
-        {
-            return PartialView();
-        }
-    }
+	public class AboutController : Controller
+	{
+		ArchitectureDbEntities db = new ArchitectureDbEntities();
+		public ActionResult Index()
+		{
+			return View();
+		}
+		public PartialViewResult SloganBanner()
+		{
+			return PartialView();
+		}
+		public ActionResult AAboutList()
+		{
+			var values = db.About.ToList();
+			return View(values);
+		}
+		[HttpGet]
+		public ActionResult AAboutUpdate(int id)
+		{
+			var aboutInfo = db.About.ToList().Find(x => x.AboutID == id);
+			return View(aboutInfo);
+		}
+		[HttpPost]
+		public ActionResult AAboutUpdate(About about)
+		{
+			var aboutInfo = db.About.ToList().Find(x => x.AboutID == about.AboutID);
+			aboutInfo.BigTitle = about.BigTitle;
+			aboutInfo.SubTitle = about.SubTitle;
+			aboutInfo.Content = about.Content;
+			aboutInfo.OurGoal = about.OurGoal;
+			aboutInfo.OurStory = about.OurStory;
+			aboutInfo.BigImage = about.BigImage;
+			aboutInfo.ShortImage = about.ShortImage;
+			db.SaveChanges();
+			return RedirectToAction("AAboutList");
+		}
+	}
 }
---
+++ ArchitectureProject/Controllers/AdminLayoutController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArchitectureProject.Controllers
+{
+    public class AdminLayoutController : Controller
+    {        
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult LeftSideMenuPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult ScriptPartial()
+        {
+            return PartialView();
+        }
+    }
+}
